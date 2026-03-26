7e76955cfa37aa3183cc84e9d43ab7701505620b
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-03-26 23:26:25 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -46,11 +46,11 @@ app.use("/api/dashboard", require("./src/routes/dashboardRoutes"));
 // app.use("*", (req, res) => res.status(404).json({ success: false, message: "Route not found" }));
 
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // wildcard api
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.use(errorHandler);

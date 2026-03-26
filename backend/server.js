f8082ddadf34905b79bff083526651ea0537b35a
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-03-26 22:51:17 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const app = express();
 
 app.use(express.static("./public"))
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || "https://shrinath-water-distributors.onrender.com", credentials: true }));
 app.use(express.json({ limit: "10kb" }));
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
